dd2e0d171732fbb98b7af3c60b6aad552f3e16cc
mystekerinos <jordy.saintalbin@gmail.com>
2024-06-20 22:19:59 +0200

    refacto-createEmployee

--- src/components/createEmployee/createEmployee.jsx
+++ src/components/createEmployee/createEmployee.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import "./CreateEmployee.css";
+import "../../assets/css/createEmployee.css";
 import Modal from "react-modal";
 import { useEmployeeStore } from "../../store/employee.store";
 import { states } from "../../store/state";
