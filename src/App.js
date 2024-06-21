9b023d82ddc8fd9041c3388d758f420a2d2a12e3
mystekerinos <jordy.saintalbin@gmail.com>
2024-06-21 20:28:19 +0200

    intall-react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-// App.js
+// src/App.js
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import CreateEmployee from "./pages/createEmployee/CreateEmployee";
 import EmployeeList from "./pages/employee/EmployeeList";
-import CreateEmployee from "./components/createEmployee/CreateEmployee";
 
-const App = () => (
-  <div>
-    <EmployeeList />
-    <CreateEmployee />
-  </div>
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/employee-list" />} />
+        <Route path="/create-employee" element={<CreateEmployee />} />
+        <Route path="/employee-list" element={<EmployeeList />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
