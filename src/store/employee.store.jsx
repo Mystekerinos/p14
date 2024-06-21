70d69903a49b3779b7504e096569d068cb76eabe
mystekerinos <jordy.saintalbin@gmail.com>
2024-06-21 20:27:14 +0200

    refacto-employee-store

--- src/store/employee.store.jsx
+++ src/store/employee.store.jsx
@@ -1,7 +1,8 @@
 import create from "zustand";
+import employeesData from "../data/employeesData";
 
 export const useEmployeeStore = create((set) => ({
-  employees: JSON.parse(localStorage.getItem("employees")) || [],
+  employees: JSON.parse(localStorage.getItem("employees")) || employeesData,
   setEmployees: (newEmployees) => {
     localStorage.setItem("employees", JSON.stringify(newEmployees));
     set({ employees: newEmployees });
